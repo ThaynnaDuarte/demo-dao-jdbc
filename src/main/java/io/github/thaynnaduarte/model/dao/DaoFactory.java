06d9095c0811696beb5fe85e5570d190e471ea07
Thaynná Duarte <thaynnaduarte14@gmail.com>
2025-07-18 15:50:34 -0300

    DepartmentDao implementation and test

--- src/main/java/io/github/thaynnaduarte/model/dao/DaoFactory.java
+++ src/main/java/io/github/thaynnaduarte/model/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package io.github.thaynnaduarte.model.dao;
 
 import io.github.thaynnaduarte.db.DB;
+import io.github.thaynnaduarte.impl.DepartmentDaoJDBC;
 import io.github.thaynnaduarte.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
